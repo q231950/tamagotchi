dd5d8ad2082c632030a29d1ed0db554f05ff1652
Martin Kim Dung-Pham <kim@elbedev.com>
2016-07-16 20:28:54 +0200

    Remove comments

--- Enterprise Tamagotchi/GameViewController.swift
+++ Enterprise Tamagotchi/GameViewController.swift
@@ -14,18 +14,13 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
+
         if let scene = GKScene(fileNamed: "HomeScene") {
-            
-            // Get the SKScene from the loaded GKScene
+
             if let sceneNode = scene.rootNode as! HomeScene? {
-                                
-                // Set the scale mode to scale to fit the window
+
                 sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
+
                 if let view = self.view as! SKView? {
                     view.presentScene(sceneNode)
                     
