298ee956e120775df2bf6af9b14d5d031f512af8
Martin Kim Dung-Pham <kim@elbedev.com>
2016-07-16 14:37:06 +0200

    Initial Commit

---
+++ Enterprise TamagotchiTests/Enterprise_TamagotchiTests.swift
@@ -0,0 +1,36 @@
+//
+//  Enterprise_TamagotchiTests.swift
+//  Enterprise TamagotchiTests
+//
+//  Created by Martin Kim Dung-Pham on 16.07.16.
+//  Copyright © 2016 Martin Kim Dung-Pham. All rights reserved.
+//
+
+import XCTest
+@testable import Enterprise_Tamagotchi
+
+class Enterprise_TamagotchiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
